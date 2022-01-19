46ed1184c1b371c91d2c7cb239dcf542444d77e5
LABCAT <shane.watters.nz@gmail.com>
2022-01-19 18:13:07 +1300

    Iniital repo setup and sketch experimentation.

---
+++ src/js/classes/AnimatedGlyph.js
@@ -0,0 +1,61 @@
+export default class AnimatedGlyph {
+    constructor(p5, x, y, width) {
+        this.p = p5;
+        this.hue = this.p.random(0, 360);
+        this.destination = this.p.createVector(x, y);
+        this.width = width;
+        this.maxWidth = width;
+        this.rotation = 0;
+        this.lifeTime = this.p.random(10000, 20000);
+        this.startTime = this.p.millis();
+        this.endTime = this.startTime + this.lifeTime;
+        
+        const randY =  this.p.random(0, this.p.height);
+        const randX =  this.p.random(0, this.p.width);
+        this.direction = this.p.random(['left', 'right', 'up', 'down']);
+        switch (this.direction) {
+            case 'left':
+                this.origin = this.p.createVector(0, randY);
+                break;
+            case 'right':
+                this.origin = this.p.createVector(this.p.width, randY);
+                break;
+            case 'up':
+                this.origin = this.p.createVector(randX, 0);
+                break;
+            case 'down':
+                this.origin = this.p.createVector(randX, this.p.height);
+                break;
+        }
+    }
+
+    update() {
+        if(this.width > 0){
+            this.width = this.width - (Math.random() / 4);
+        }
+        this.rotation++;
+    }
+
+    draw() {
+        const x1 = 0 - (this.width/2),   
+            y1 = 0 + (this.width/2), 
+            x2 = 0,
+            y2 = 0 - (this.width/2),
+            x3 = 0 + (this.width/2), 
+            y3 = 0 + (this.width/2),
+            currentTime = this.p.millis();
+        if(currentTime < this.endTime){
+            const scale = this.p.min(1, (currentTime - this.startTime) / (this.endTime - this.startTime)),
+                dist = window.p5.Vector.sub(this.destination, this.origin).mult(scale),
+                pos = window.p5.Vector.add(this.origin, dist);
+            this.p.translate(pos.x, pos.y);
+            this.p.rotate(this.rotation);
+            this.p.strokeWeight(4);
+            this.p.stroke(this.hue, 100, 100);
+            this.p.fill(this.hue, 100, 100, 0.25);
+            this.p.triangle(x1, y1, x2, y2, x3, y3);
+            this.p.rotate(-this.rotation);
+            this.p.translate(-pos.x, -pos.y);
+        }
+    }
+}
